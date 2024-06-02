9ab584d9abac8c285e6836e9b51e8428c77161f6
Muhammad Surya Adhi Setiawan <21082010167@student.upnjatim.ac.id>
2024-06-02 23:08:45 +0700

    Updated MainActivity UI.

--- app/src/main/java/com/litcove/litcove/ui/main/profile/ProfileViewModel.kt
+++ app/src/main/java/com/litcove/litcove/ui/main/profile/ProfileViewModel.kt
@@ -1,13 +1,13 @@
-package com.litcove.litcove.ui.dashboard
+package com.litcove.litcove.ui.main.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is profile Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/litcove/litcove/ui/main/search/SearchViewModel.kt
+++ app/src/main/java/com/litcove/litcove/ui/main/search/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package com.litcove.litcove.ui.home
+package com.litcove.litcove.ui.main.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is search Fragment"
     }
     val text: LiveData<String> = _text
 }
